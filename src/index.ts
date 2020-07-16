6a99c161293e94c0d553e6d4e746836c54631c88
Luca Scaringella <lucalstyle@gmail.com>
2020-07-16 12:43:17 +0200

    Export all important types and classes.

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+import {JSONString} from "./lib/JsonUtils";
+import WriteStream from "./lib/WriteStream";
+import Communicator from "./lib/Communicator";
+import ReadStream from "./lib/ReadStream";
+import {StreamCloseCode} from "./lib/StreamCloseCode";
+import {StreamState} from "./lib/StreamState";
+import MixedJSON from "./lib/MixedJSON";
+import {TimeoutError,TimeoutType,InvalidActionError,ConnectionLostError} from "./lib/Errors";
+
+export {
+    Communicator,
+    JSONString,
+    MixedJSON,
+    WriteStream,
+    ReadStream,
+    StreamCloseCode,
+    StreamState,
+    TimeoutError,
+    TimeoutType,
+    InvalidActionError,
+    ConnectionLostError
+}
