873b0dfc050b1fc12cb4a461c4508e483368b3fe
Luca Scaringella <lucalstyle@gmail.com>
2020-07-16 12:32:32 +0200

    Implement types for the Ziron protocol.

---
+++ src/lib/Protocol.ts
@@ -0,0 +1,100 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+export const enum PacketType {
+    Bundle,
+    Transmit,
+    Invoke,
+    InvokeDataResp,
+    StreamAccept,
+    StreamChunk,
+    ReadStreamClose,
+    WriteStreamClose,
+    BinaryReference,
+    InvokeErrResp
+}
+
+export const enum DataType {
+    JSON,
+    Binary,
+    Stream,
+    MixedJSON
+}
+
+/**
+ * Indexes:
+ * 0: PacketType
+ * 1: Event
+ * 2: DataType
+ * 3: Data
+ */
+export type TransmitPacket = [PacketType.Transmit,string,DataType,any];
+
+/**
+ * Indexes:
+ * 0: PacketType
+ * 1: Event
+ * 2: CallId
+ * 3: DataType
+ * 4: Data
+ */
+export type InvokePacket = [PacketType.Invoke,string,number,DataType,any];
+
+/**
+ * Indexes:
+ * 0: PacketType
+ * 1: CallId
+ * 2: Err
+ */
+export type InvokeErrRespPacket = [PacketType.InvokeErrResp,number,any];
+
+/**
+ * Indexes:
+ * 0: PacketType
+ * 1: CallId
+ * 2: DataType
+ * 3: Data
+ */
+export type InvokeDataRespPacket = [PacketType.InvokeDataResp,number,DataType,any];
+
+/**
+ * Indexes:
+ * 0: PacketType
+ * 1: StreamId
+ */
+export type StreamAcceptPacket = [PacketType.StreamAccept,number];
+
+/**
+ * Indexes:
+ * 0: PacketType
+ * 1: StreamId
+ * 2: DataType
+ * 3: Data
+ */
+export type StreamChunkPacket = [PacketType.StreamChunk,number,DataType,any];
+
+/**
+ * Indexes:
+ * 0: PacketType
+ * 1: StreamId
+ * 2: code
+ * 3?: DataType
+ * 4?: Data
+ */
+export type WriteStreamClosePacket = [PacketType.WriteStreamClose,number,number,DataType | undefined,any | undefined];
+
+/**
+ * Indexes:
+ * 0: PacketType
+ * 1: StreamId
+ * 2: code
+ */
+export type ReadStreamClosePacket = [PacketType.ReadStreamClose,number,number];
+
+export type ActionPacket = TransmitPacket | InvokePacket | InvokeErrRespPacket |
+    InvokeDataRespPacket | StreamAcceptPacket | StreamChunkPacket |
+    WriteStreamClosePacket | ReadStreamClosePacket ;
+export type BundlePacket = [PacketType.Bundle,ActionPacket[]];
