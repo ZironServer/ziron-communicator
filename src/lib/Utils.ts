c06f5d6435bf02e119cc0e4d03abc388a64058e1
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2021-11-30 00:02:46 +0100

    Implement guessStringSize util function.

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -6,3 +6,11 @@ Copyright(c) Ing. Luca Gian Scaringella
 
 export type Writable<T> = { -readonly [P in keyof T]: T[P] };
 export const RESOLVED_PROMISE = Object.freeze(Promise.resolve()) as Promise<void>;
+
+/**
+ * Guesses the string max byte size in UTF-8.
+ * @param str
+ */
+export function guessStringSize(str: string) {
+    return str.length * 4;
+}
